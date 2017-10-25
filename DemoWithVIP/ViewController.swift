58bc946b30ef2b0a7aa06b2d9c1f0d1e2b48bb0a
Lam Luong <luongnhatlam94@gmail.com>
2017-10-25 14:42:57 +0700

    Initial Commit

---
+++ DemoWithVIP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoWithVIP
+//
+//  Created by Lương Nhật Lâm on 10/25/17.
+//  Copyright © 2017 Lương Nhật Lâm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
